45a9610590c68a5e871cbe80d5914ac338783d26
devjagz <jgomezpersonal7@gmail.com>
2023-12-06 00:14:19 -0500

    feat: show stars as background

---
+++ src/app/shared/components/stars/stars.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stars',
+  standalone: true,
+  imports: [],
+  templateUrl: './stars.component.html',
+  styleUrl: './stars.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class StarsComponent {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileImageComponent } from './components/profile-image/profile-image.component';
+import { StarsComponent } from './components/stars/stars.component';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, ProfileImageComponent],
-  exports: [ProfileImageComponent],
+  imports: [CommonModule, ProfileImageComponent, StarsComponent],
+  exports: [ProfileImageComponent, StarsComponent],
 })
 export class SharedModule {}
